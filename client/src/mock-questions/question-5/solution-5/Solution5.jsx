f39dacb98cd94780e847ad0f116aefe8b51d3454
Joseph Boulos <jab12288@gmail.com>
2023-10-05 16:07:31 -0700

    iniitla solution5 component setup

--- client/src/mock-questions/question-5/solution-5/Solution5.jsx
+++ client/src/mock-questions/question-5/solution-5/Solution5.jsx
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router';
 
 export const Solution5 = () => {
+  const [users, setUsers] = useState([]);
+  const [filteredUsers, setFilteredUsers] = useState([]);
+  const [searchString, setSearchString] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch('https://jsonplaceholder.typicode.com/users');
+        const data = await res.json();
+        console.log(data);
+        setUsers(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
   const navigate = useNavigate();
   const goBack = () => {
-    navigate('/')
-  }
+    navigate('/');
+  };
+
   return (
     <div>
       <button onClick={goBack}>Back</button>
-      <h1>Question 5</h1>
+
+      <input
+        placeholder='search...'
+        onChange={(e) => setSearchString(e.target.value)}
+        style={{ display: 'block', marginTop: '15px' }}
+      />
+      <ul>
+        {users.map((user) => (
+          <li>{user.name}</li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
