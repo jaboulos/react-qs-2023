3cec77d5ad2efcdaa51c0a6e938e76b6a23a7421
Joseph Boulos <jab12288@gmail.com>
2023-10-11 15:07:30 -0700

    add add task functionality

--- client/src/App.js
+++ client/src/App.js
@@ -8,14 +8,14 @@ import { Solution3 } from './mock-questions/question-3/solution-3/Solution3';
 import { Solution4a } from './mock-questions/question-4/solution-4a/Solution4a';
 import { Solution4b } from './mock-questions/question-4/solution-4b/Solution4b';
 import { Solution5 } from './mock-questions/question-5/solution-5/Solution5';
-import { ContextExample } from './mock-questions/question-misc-context-ex/ContextExample';
+// import { ContextExample } from './mock-questions/question-misc-context-ex/ContextExample';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<HomePage />} />
-        <Route path='/login' element={<ContextExample />} />
+        {/* <Route path='/login' element={<ContextExample />} /> */}
         <Route path='/q1' element={<Solution1a />} />
         <Route path='/q2' element={<Solution2 />} />
         <Route path='/q3' element={<Solution3 />} />
